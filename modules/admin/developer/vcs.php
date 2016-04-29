aec31e4e62f83d8f383737b5d65fbdd1fa99af03
Vishva Kumara <vishva@villvay.com>
2016-04-29 16:53:50 +0530

    VCS stub

---
+++ modules/admin/developer/vcs.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL_STATIC; ?>css/datatable.css" />
+<h2>Version Control System</h2>
+
+<p>This allows you synchronize file changes between two or more deployments of a Veev app.</p>
